861b8448b6eef2d7cfef171720b2e2f0ebca456e
SeYunJung <77439732+SeYunJung@users.noreply.github.com>
2025-08-12 22:21:14 +0900

    [Update] Header, 주석 수정

--- Assets/Scripts/UI/Condition.cs
+++ Assets/Scripts/UI/Condition.cs
@@ -4,29 +4,29 @@ using UnityEngine.UI;
 
 public class Condition : MonoBehaviour
 {
-    [Header("체력 변수들")]
-    public float curValue; // 현재 체력 
-    public float startValue; // 시작 체력 
-    public float maxValue; // 최대 체력
+    [Header("체력이나 스피드 변수들")]
+    public float curValue; // 현재 체력/스피드 
+    public float startValue; // 시작 체력/스피드 
+    public float maxValue; // 최대 체력/스피드
     public float passiveValue; // 주기적으로 변하는 값 (몇씩 체력이 줄어들지)
     public Image uiBar; // 이미지에 있는 FillAmount를 컨트롤하려면 image가 필요함.
     
     void Start()
     {
-        // 처음에는 시작체력부터 시작 
+        // 처음에는 시작체력/스피드부터 시작 
         curValue = startValue; 
     }
 
     void Update()
     {
-        // 체력바 UI 업데이트 
+        // 체력/스피드바 UI 업데이트 
         uiBar.fillAmount = GetPercentage();
     }
 
     // FillAmount 값을 계산해서 반환해주는 메서드 
     float GetPercentage()
     {
-        return curValue / maxValue; // curValue만 수정되면 Update에서 자동으로 UI가 업디이트된다. 
+        return curValue / maxValue; // curValue만 수정되면 Update에서 자동으로 UI가 업데이트된다. 
     }
 
     // curValue 업데이트 메서드들 -> 외부에서 호출 -> Update에서 현재 체력이 업데이트됨. 
